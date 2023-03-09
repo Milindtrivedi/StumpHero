dde7b5915b6b852f8943d0ab87c89eeb6507dfd0
Milind Trivedi <milindtrivedi47@gmail.com>
2023-03-09 16:35:44 +0530

    Loading effect added and minor other changes

---
+++ StumpHero/StumpHero/Views/Loader/AppLoader.swift
@@ -0,0 +1,51 @@
+//
+//  AppLoader.swift
+//  StumpHero
+//
+//  Created by Apple on 09/03/23.
+//
+
+import SwiftUI
+
+struct AppLoader: View {
+    
+    @State private var drawingStroke = false
+    let customloadingclr = Color(#colorLiteral(red: 0.9725490196, green: 0.6078431373, blue: 0.02745098039, alpha: 1))
+    let animation = Animation
+        .easeOut(duration: 3)
+        .repeatForever(autoreverses: false)
+        .delay(0.5)
+    
+    var body: some View {
+        ZStack {
+            ring(for: customloadingclr)
+                .frame(width: UIScreen.screenWidth * 0.4)
+        }
+        .animation(animation, value: drawingStroke)
+        .onAppear {
+            drawingStroke.toggle()
+        }
+        .ignoresSafeArea()
+    }
+    
+    func ring(for color: Color) -> some View {
+        // Background ring
+        Circle()
+            .stroke(style: StrokeStyle(lineWidth: 16))
+            .foregroundStyle(.tertiary)
+            .overlay {
+                // Foreground ring
+                Circle()
+                    .trim(from: 0, to: drawingStroke ? 1 : 0)
+                    .stroke(color.gradient,
+                            style: StrokeStyle(lineWidth: 16, lineCap: .round))
+            }
+            .rotationEffect(.degrees(-90))
+    }
+}
+
+struct AppLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        AppLoader()
+    }
+}
--- StumpHero/StumpHero/Views/LoginView/LoginView.swift
+++ StumpHero/StumpHero/Views/LoginView/LoginView.swift
@@ -9,99 +9,113 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @ObservedObject var viewModel = LoginViewModel()
+    //MARK: - PROPERTIES
     
+    @ObservedObject var viewModel = LoginViewModel()
     @AppStorage("isOnboarding") var isOnboarding : Bool = true
     private var borderColour = Color(hex: "E4E5E5")
     @State private var isSecured: Bool = true
     
+    
+    //MARK: - BODY
     var body: some View {
         NavigationView {
-        GeometryReader { geo in
-            ScrollView {
-                VStack(alignment: .center, spacing: 16.0) {
-                    TextField("", text: $viewModel.username)
-                        .placeholder(when: viewModel.username.isEmpty) {
-                            Text("Username").foregroundColor(Color(hex: "AAAAAA"))
-                                .font(.system(size: 18, weight: .bold, design: .default))
-                            
-                        }
-                        .keyboardType(.namePhonePad)
-                        .frame(height: 55)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .padding([.horizontal], 16)
-                        .cornerRadius(7)
-                        .overlay(RoundedRectangle(cornerRadius: 7).stroke(viewModel.isOk ? borderColour : .red))
-                    
-                    ZStack(alignment: .trailing) {
-                        Group {
-                            if isSecured {
-                                SecureField("", text: $viewModel.password)
-                                    .placeholder(when: viewModel.password.isEmpty) {
-                                        Text("Password").foregroundColor(Color(hex: "AAAAAA"))
-                                            .font(.system(size: 18, weight: .bold, design: .default))
-                                    }
+            GeometryReader { geo in
+                ZStack {
+                    VStack(alignment: .center, spacing: 16.0) {
+                        TextField("", text: $viewModel.username)
+                            .placeholder(when: viewModel.username.isEmpty) {
+                                Text("Username").foregroundColor(Color(hex: "AAAAAA"))
+                                    .font(.system(size: 18, weight: .bold, design: .default))
                                 
-                                    .frame(height: 55)
-                                    .textFieldStyle(PlainTextFieldStyle())
-                                    .padding([.leading], 16)
-                                    .padding([.trailing], 50)
-                                    .cornerRadius(7)
-                                    .overlay(RoundedRectangle(cornerRadius: 7).stroke(borderColour))
-                            } else {
-                                TextField("", text: $viewModel.password)
-                                    .placeholder(when: viewModel.password.isEmpty) {
-                                        Text("Password").foregroundColor(Color(hex: "AAAAAA"))
-                                            .font(.system(size: 18, weight: .bold, design: .default))
-                                    }
-                                
-                                    .frame(height: 55)
-                                    .textFieldStyle(PlainTextFieldStyle())
-                                    .padding([.leading], 16)
-                                    .padding([.trailing], 50)
-                                    .cornerRadius(7)
-                                    .overlay(RoundedRectangle(cornerRadius: 7).stroke(borderColour))
                             }
+                            .keyboardType(.namePhonePad)
+                            .frame(height: 55)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .padding([.horizontal], 16)
+                            .cornerRadius(7)
+                            .overlay(RoundedRectangle(cornerRadius: 7).stroke(viewModel.isOk ? borderColour : .red))
+                            .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) {
+                                    Spacer()
+                                    Button("Done") {
+                                        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                                            let keyWindow = windowScene.windows.filter { $0.isKeyWindow }.first
+                                            keyWindow?.endEditing(true)
+                                        }
+                                    }.padding(.trailing, 16)
+                                }
+                            }
+                        
+                        ZStack(alignment: .trailing) {
+                            Group {
+                                if isSecured {
+                                    SecureField("", text: $viewModel.password)
+                                        .placeholder(when: viewModel.password.isEmpty) {
+                                            Text("Password").foregroundColor(Color(hex: "AAAAAA"))
+                                                .font(.system(size: 18, weight: .bold, design: .default))
+                                        }
+                                    
+                                        .frame(height: 55)
+                                        .textFieldStyle(PlainTextFieldStyle())
+                                        .padding([.leading], 16)
+                                        .padding([.trailing], 50)
+                                        .cornerRadius(7)
+                                        .overlay(RoundedRectangle(cornerRadius: 7).stroke(borderColour))
+                                } else {
+                                    TextField("", text: $viewModel.password)
+                                        .placeholder(when: viewModel.password.isEmpty) {
+                                            Text("Password").foregroundColor(Color(hex: "AAAAAA"))
+                                                .font(.system(size: 18, weight: .bold, design: .default))
+                                        }
+                                    
+                                        .frame(height: 55)
+                                        .textFieldStyle(PlainTextFieldStyle())
+                                        .padding([.leading], 16)
+                                        .padding([.trailing], 50)
+                                        .cornerRadius(7)
+                                        .overlay(RoundedRectangle(cornerRadius: 7).stroke(borderColour))
+                                }
+                            }
+                            
+                            Button(action: {
+                                isSecured.toggle()
+                            }) {
+                                Image(systemName: self.isSecured ? "eye.slash.fill" : "eye.fill")
+                                    .accentColor(.gray)
+                            }.padding(.trailing, 16)
                         }
                         
-                        Button(action: {
-                            isSecured.toggle()
-                        }) {
-                            Image(systemName: self.isSecured ? "eye.slash.fill" : "eye.fill")
-                                .accentColor(.gray)
-                        }.padding(.trailing, 16)
-                    }
+                        Button {
+                            viewModel.login()
+                        } label: {
+                            Text("Sign In")
+                                .font(.system(size: 18, weight: .bold, design: .default))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .frame(height: 55)
+                                .frame(maxWidth: .infinity)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 7)
+                                        .fill(Color(hex: "4399E6"))
+                                    //.shadow(color: .white, radius: 2, x: 0, y: 2)
+                                )
+                        }
+                        .navigationDestination(isPresented: $viewModel.loggedIn) {
+                            Dashboard()
+                        }
+                    }.padding([.horizontal], 24)
+                        .position(x: geo.size.width / 2, y: geo.size.height / 2)
                     
-                    Button {
-                        viewModel.login()
-                    } label: {
-                        Text("Sign In")
-                            .font(.system(size: 18, weight: .bold, design: .default))
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .frame(height: 55)
-                            .frame(maxWidth: .infinity)
-                            .background(
-                                RoundedRectangle(cornerRadius: 7)
-                                    .fill(Color(hex: "4399E6"))
-                                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
-                            )
-                    }
-                    .navigationDestination(isPresented: $viewModel.loggedIn) {
-                        Dashboard()
-                    }
-                }.padding([.horizontal], 24)
-                    .position(x: geo.size.width / 2, y: geo.size.height / 2)
+                }
+                .navigationBarTitle("Sign in")
+                .navigationBarHidden(false)
                 
+                .background(
+                    Color(hex: AppConstants.ViewBackGroundClr)
+                )
             }
-            .navigationBarTitle("Sign in")
-            .navigationBarHidden(false)
-            
-            .background(
-                Color.white
-            )
         }
-    }
         
         .alert(isPresented: $viewModel.showErrorPopup) {
             Alert(title: Text(viewModel.error),
@@ -112,9 +126,22 @@ struct LoginView: View {
             }
             )
         }
+        
+        .overlay {
+            if viewModel.isLoading {
+                ZStack {
+                    Color(white: 0, opacity: 0.55)
+                    AppLoader()
+                }.ignoresSafeArea(.all)
+                    .onTapGesture {
+                        viewModel.isLoading = false
+                    }
+            }
+        }
     }
 }
 
+//MARK: - PREVIEW
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
